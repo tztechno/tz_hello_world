b9b179e549ed904c59535fecb34a3f92c57c8e00
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-15 11:50:21 +0900

    Create hello.dart

---
+++ hello.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, World!'),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    ),
+  );
+}
