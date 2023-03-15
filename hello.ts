d17599c8361c52036cb9d364642166cffd55860f
tztechno <47345279+tztechno@users.noreply.github.com>
2023-03-15 11:40:01 +0900

    Create hello.ts

---
+++ hello.ts
@@ -0,0 +1,7 @@
+// define a function that takes in a string and returns a string
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+// call the greet function and log the result to the console
+console.log(greet('World'));
